8459e6ea2f9b21e5c58e1d8cd2934f7a0847cfbe
Luna Kilborn <leland.kilborn@hotmail.com>
2023-04-04 16:48:10 -0400

    Add graphics module
    Really basic, just a proof of concept

---
+++ graphics/src/lib.rs
@@ -0,0 +1,7 @@
+extern crate sdl2;
+
+pub mod initialization;
+mod state_management;
+pub use state_management::*;
+mod frames;
+pub use frames::*;
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
 mod random;
 
 fn main() {
-    let mut gen = random::Generator::new(7, 10);
-    for _ in 0..100 {
-        println!("{:x}", gen.next());
+    let graphics_state_result = graphics::initialization::initialize(String::from("Test Window"));
+
+    let mut graphics_state = match graphics_state_result {
+        Err(e) => {
+            println!("ERROR: {:}", e);
+            std::process::exit(1)
+        }
+        Ok(gs) => gs,
+    };
+
+    loop {
+        handle_frame_result(graphics::do_frame(&mut graphics_state));
+    }
+}
+
+fn handle_frame_result(frame: graphics::FrameResult) {
+    match frame {
+        graphics::FrameResult::Normal => {}
+        graphics::FrameResult::CloseProgram => std::process::exit(0),
+        graphics::FrameResult::Exception => std::process::exit(1),
     }
 }
